88d4bb9d12478d5b8bcfb77c3afd225de1ad89dd
phapdev <luongphap1810@gmail.com>
2025-06-04 14:49:04 +0700

    fix plugin and added rust, icon, tresitter, tmux, comment, lspconfig

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,13 +1,5 @@
-local plugin_files = {
-  "conform",
-  "kitty",
-  "nvim-lspconfig",
-  "treesister"
+return {
+  {
+    "folke/lazy.nvim", version = "*"
+  }
 }
-
-for _, name in ipairs(plugin_files) do
-  local ok, err = pcall(require, "plugins." .. name)
-  if not ok then
-    vim.notify("Error loading plugin: " .. name .. "\n" .. err, vim.log.levels.ERROR)
-  end
-end
