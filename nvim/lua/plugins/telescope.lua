1b44346497378c45e28d6696b679f20759e5b1e5
phapdev <luongphap1810@gmail.com>
2025-06-05 01:24:12 +0700

    nvim: fix telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,11 +7,11 @@ return {
       local builtin = require('telescope.builtin')
       local wk = require('which-key')
       wk.register({
-        ['ff'] = { builtin.find_files, "Find File" },
-        ['fb'] = { builtin.buffers, "Find Buffer" },
-        ['fg'] = { builtin.live_grep, "Find with Grep" },
-        ['fh'] = { builtin.help_tags, "Find Help" },
-        ['fn'] = { ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "File Browser" },
+        ['<leader>ff'] = { builtin.find_files, "Find File" },
+        ['<leader>fb'] = { builtin.buffers, "Find Buffer" },
+        ['<leader>fg'] = { builtin.live_grep, "Find with Grep" },
+        ['<leader>fh'] = { builtin.help_tags, "Find Help" },
+        ['<leader>fn'] = { ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "File Browser" },
       }, { prefix = "<leader>" })
     end,
     opts = function()
