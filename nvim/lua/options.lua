d2e17a6b27feb48fe9b939ed7c25a3314b559cb0
phapdev <luongphap1810@gmail.com>
2025-06-06 14:36:41 +0700

    new config for rust lang

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,12 +2,13 @@ require "nvchad.options"
 
 -- add yours here!
 
-local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- Numbers
 o.number = true
 o.numberwidth = 2
 o.ruler = false
 o.relativenumber = true
+
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,20 @@
 return {
   {
-    "folke/lazy.nvim", version = "*"
-  }
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
