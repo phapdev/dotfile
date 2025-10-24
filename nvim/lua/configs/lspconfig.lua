ca2448b08fc8f874f0a5c843f4a2e52b4e9cfe4c
phapdev <luongphap1810@gmail.com>
2025-10-24 16:33:06 +0700

    add treesitter, bank-line, lspconfig

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,53 @@
-require("nvchad.configs.lspconfig").defaults()
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+-- local lspconfig = require("lspconfig") -- pre nvim 0.11
+local lspconfig = require("nvchad.configs.lspconfig") -- nvim 0.11
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- list of all servers configured.
+lspconfig.servers = {
+  "lua_ls",
+  "html-ls"
+}
+
+-- list of servers configured with default config.
+local default_servers = {}
+
+-- lsps with default config
+for _, lsp in ipairs(default_servers) do
+  -- lspconfig[lsp].setup({ -- pre nvim 0.11
+  vim.lsp.config(lsp, { -- nvim 0.11
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  })
+end
+
+-- lspconfig.lua_ls.setup({ -- pre nvim 0.11
+vim.lsp.config("lua_ls", { -- nvim 0.11
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = false, -- Disable all diagnostics from lua_ls
+        -- globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          vim.fn.expand("$VIMRUNTIME/lua"),
+          vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+          vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/love2d/library",
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+})
+-- read :h vim.lsp.config for changing options of lsp servers
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvchad.configs.lspconfig").defaults()
+    require("configs.lspconfig")
+  end,
+}
